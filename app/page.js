ec11bddd5567ff584b6a544ff23991b344232496
sjhong98 <sjhong98@icloud.com>
2023-11-02 12:47:00 +0900

    feat: main page 구현

--- app/page.js
+++ app/page.js
@@ -1,113 +1,116 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import { useState, useEffect, useRef } from "react";
+import image1 from './assets/images/image.png';
+import image2 from './assets/images/image2.png';
+import Image from "next/image";
+import './page.css';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Main() {
+  const image1Ref = useRef();
+  const section1Ref = useRef();
+  const tx1Ref = useRef();
+  const tx2Ref = useRef();
+  const tx2_1Ref = useRef();
+  const tx2_2Ref = useRef();
+  const tx3Ref = useRef();
+
+  useEffect(() => {  // tx1 move
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      markers: false,
+      trigger: '.text1',
+      start: 'top 30%', 
+      end: 'bottom 0%',  
+      scrub: 3,
+      onUpdate: (self) => {
+        gsap.to(tx1Ref.current, {
+            y: (self.progress * -1000),
+            overwrite: true,
+        });
+      },
+    });
+    ScrollTrigger.update();
+  }, [])
+
+  useEffect(() => {  // tx3 move
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      markers: false,
+      trigger: '.text1',
+      start: 'top 30%', 
+      end: 'bottom 0%',  
+      scrub: 3,
+      onUpdate: (self) => {
+        gsap.to(tx3Ref.current, {
+            y: (self.progress * -2000),
+            overwrite: true,
+        });
+      },
+    });
+    ScrollTrigger.update();
+  }, [])
+
+  useEffect(() => {  // tx2 move
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      markers: true,
+      trigger: '.text1',
+      start: 'top 30%', 
+      end: 'bottom 0%',  
+      scrub: 3,
+      onUpdate: (self) => {
+        gsap.to(tx2Ref.current, {
+          y: (self.progress * -100),
+            overwrite: true,
+        });
+      },
+    });
+    ScrollTrigger.update();
+  }, [])
+
+  useEffect(() => {  // tx2 move
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      markers: false,
+      trigger: '.text1',
+      start: '50% 40%', 
+      end: '70% 30%',  
+      scrub: 3,
+      onUpdate: (self) => {
+        gsap.to(tx2_2Ref.current, {
+          opacity: 0+self.progress
+        });
+      },
+    });
+    ScrollTrigger.update();
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="section0 w-screen h-[130vh] bg-theme">
+      <Image src={image2} alt="..." className="fixed w-[12vw] mt-12 ml-12 cursor-pointer z-50" style={{transform:"rotate(-15deg)"}} />
+      <div ref={section1Ref} className="section1 w-screen h-screen flex flex-col justify-center items-center">
+        <Image src={image1} alt="..." className="myImage fixed opacity-30 z-10 w-1/4 mr-[2vw] show-up-img" style={{transform:"rotate(-10deg)"}}/>
+        <div className="text1 w-1/2 h-1/3 flex flex-col justify-center ml-[8vw] z-20">
+          <div ref={tx1Ref}>
+            <p className="text-black text-[5rem] mb-6 show-up-1 transform translate-y-[1000px]" style={{fontFamily:'korean'}}>안녕하세요</p>
+          </div>
+          <div ref={tx2Ref}>
+            <p className="text-black text-[6rem] leading-[70px] show-up-2 transform translate-y-[1000px]" style={{fontFamily:'akira'}}>FRONTEND</p>
+            <p className="text-black text-[6rem] leading-[70px] show-up-2 transform translate-y-[1000px]" style={{fontFamily:'akira'}}>DEVELOPER</p>
+            <div ref={tx2_2Ref} className="opacity-0">
+              <p className="text-black text-[6rem] leading-[70px] show-up-2 transform translate-y-[1000px]" style={{fontFamily:'akira'}}>PORTFOLIO</p>
+            </div>
+          </div>
+          <div ref={tx3Ref}>
+            <p className="text-black text-[5rem] mt-6 show-up-3 transform translate-y-[1000px]" style={{fontFamily:'korean'}}>홍승재 입니다</p>
+          </div>
+          
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'theme': '#FEE832',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
